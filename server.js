1b23b5627ea63b5178631ce9ba1b6897eab65808
Eerik Lehtomäki <eejujole@student.jyu.fi>
2018-10-08 00:31:29 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express'),
+  path = require('path');
+
+const app = express();
+
+app.use(express.static('/dist/ng6'));
+
+app.get('/*', (req,res) =>{
+  res.sendFile(path.join(__dirname, '/dist/ng6/index.html'));
+});
+
+app.listen(process.env.port || 8080,() => console.log('Running...'));
